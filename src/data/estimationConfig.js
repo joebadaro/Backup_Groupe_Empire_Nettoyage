ea2c8f4174e01743f91870f1a5cfe9a61268337e
empirenettoyage-ops <empirenettoyage@gmail.com>
2026-01-09 16:01:32 -0500

    boxes estimate commercial

--- src/data/estimationConfig.js
+++ src/data/estimationConfig.js
@@ -351,7 +351,21 @@ After discussing with you, we will be able to give you a more accurate estimate 
                     label: { fr: "Surface Totale (pi²)", en: "Total Area (sq ft)" },
                     type: "input_number",
                     desc: { fr: "Entrez la surface à nettoyer", en: "Enter area to clean" },
-                    price: 0 // Price is 0 because it's quote only
+                    price: 0
+                },
+                {
+                    id: "comm_rooms",
+                    label: { fr: "Nombre de pièces", en: "Number of Rooms" },
+                    type: "input_number",
+                    desc: { fr: "Bureaux, corridors, salles...", en: "Offices, hallways, rooms..." },
+                    price: 0
+                },
+                {
+                    id: "comm_stairs",
+                    label: { fr: "Cages d'escalier", en: "Staircases" },
+                    type: "input_number",
+                    desc: { fr: "Nombre de volées d'escaliers", en: "Number of flights of stairs" },
+                    price: 0
                 }
             ]
         }
